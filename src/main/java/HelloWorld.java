6629724a066543d981e68d0bb5002d1985ae961e
Bern00 <ecsbern@gmail.com>
2020-11-04 13:54:09 +0100

    Hello GitHub!

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello Idea and Git!");
+        System.out.println("Hello Idea and GitHub!");
     }
 }
